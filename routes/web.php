eeec97bca597610c39c32a822bbafef5a46a2ecd
Oscar <97081587@st.deltion.nl>
2026-06-02 11:30:55 +0200

    refactor: remove commented-out route for Inertia.js Home page

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,4 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home;
 
-// Route::get('/', function () {
-//     return Inertia::render('Home', [
-//         // 'canLogin' => Route::has('login'),
-//         // 'canRegister' => Route::has('register'),
-//     ]);
-// });
-
 Route::get('/', [Home::class, 'index']);
