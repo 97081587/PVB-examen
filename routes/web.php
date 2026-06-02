1924ea17cb6651ff7c462c3e0b183a2c9235d08e
Oscar <97081587@st.deltion.nl>
2026-06-02 14:10:55 +0200

    feat: add routes for klantregistratie and store method in klantRegistratieController

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\klantRegistratieController;
 // Route::get('/', [Home::class, 'index']);
 
 Route::get('/klantregistratie', [klantRegistratieController::class, 'index'])->name('klant-registratie');
+
+Route::post('/klantregistratie', [klantRegistratieController::class, 'store'])->name('klant-registratie.store');
