6edf6528c08a116c080d0a73490b25760f236209
Oscar <97081587@st.deltion.nl>
2026-06-02 12:15:56 +0200

    feat: add route for klant-registratie to access registration view

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home;
 
 Route::get('/', [Home::class, 'index']);
+
+Route::get('/klant-registratie', [klantRegistratieController::class, 'index'])->name('klant-registratie');
