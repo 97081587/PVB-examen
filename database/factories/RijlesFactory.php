5626e8b6818d848187b140af835e3124a8ab947b
Oscar <97081587@st.deltion.nl>
2026-06-08 09:27:35 +0200

    fix: update lesson status assignment logic in RijlesFactory for accurate past status handling

--- database/factories/RijlesFactory.php
+++ database/factories/RijlesFactory.php
@@ -32,25 +32,28 @@ class RijlesFactory extends Factory
             'exam_info' => $this->faker->sentence(),
             'lesson_funds' => $this->faker->sentence(),
             'instructor_name' => User::where('role', 'instructeur')->inRandomOrder()->first()->first_name . ' ' . User::where('role', 'instructeur')->inRandomOrder()->first()->last_name,
-            'status' => fake()->randomElement(['gepland', 'afgerond', 'geannuleerd']),
+            'status'= $lessonDateTime->isPast()
+                ? 'afgerond'
+                : fake()->randomElement([
+                    'gepland',
+                    'gepland',
+                    'gepland',
+                    'geannuleerd',
+                ]),
             'note' => '',
         ];
 
         
-        $date = fake()->dateTimeBetween('2026-06-01', '2026-06-30');
-        $startTime = fake()->time('H:i');
+        // $date = fake()->dateTimeBetween('2026-06-01', '2026-06-30');
+        // $startTime = fake()->time('H:i');
 
-        $lessonDateTime = Carbon::parse(
-        $date->format('Y-m-d') . ' ' . $startTime
-        );
+        // $lessonDateTime = Carbon::parse(
+        // $date->format('Y-m-d') . ' ' . $startTime
+        // );
 
         //checkt of de les in het verleden ligt. Als dat zo is, markeert het als 'afgerond', anders krijgt het een willekeurige status.
         return [
-            'date' => $date->format('Y-m-d'),
-            'start_time' => $startTime,
-            'status' => $lessonDateTime->isPast()
-                ? 'afgerond'
-                : 'gepland',
+            'status' => $status,
         ];
     }
 }
